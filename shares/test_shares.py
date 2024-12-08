17e5c84d38091725de51c5936ce62118cd159198
Gizachew Bayness <eleccrazy24@gmail.com>
2024-12-08 01:09:56 +0100

    adding gitignore file

---
+++ shares/test_shares.py
@@ -0,0 +1,11 @@
+import pytest
+from shares import (
+    build_graph,
+    find_wccs,
+    detect_cycle,
+    process_tree_component,
+    process_cycle_component,
+    find_max_shares,
+    read_input_from_file,
+    write_output_to_file
+)
